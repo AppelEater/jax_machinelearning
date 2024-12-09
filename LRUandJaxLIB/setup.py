4a143011eefcb0820f78ddef0f7efc2c9eba6d43
David Johansen <David.Johansen@ext.esa.int>
2024-12-09 17:36:52 +0100

    Added the ML as a library to make writing code for it easier

---
+++ LRUandJaxLIB/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="LRU and JAX library",
+    version="0.1.0",
+    description="A local library used to calculated the LRU and JAX library.",
+    packages=find_packages(),
+    python_requires='==3.10.11',
+)
